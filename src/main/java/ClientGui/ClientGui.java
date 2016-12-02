75487dbe8607f403787697bdb30a1fcb753d0107
Charkin Kostya <93kostya@gmail.com>
2016-12-02 15:58:52 +0300

    init client gui

---
+++ src/main/java/ClientGui/ClientGui.java
@@ -0,0 +1,40 @@
+package ClientGui;/**
+ * Created by kostya on 02.12.2016.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientGui extends Application {
+    private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("client-gui.fxml"));
+        Parent root = loader.load();
+
+        ((ClientGuiController)loader.getController()).setMainApp(this);
+
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("tracker-icon.png")));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Tracker");
+        primaryStage.show();
+    }
+
+    Stage getPrimaryStage() {
+        return primaryStage;
+    }
+}
---
+++ src/main/java/ClientGui/ClientGuiController.java
@@ -0,0 +1,13 @@
+package ClientGui;
+
+/**
+ * Created by kostya on 02.12.2016.
+ */
+
+public class ClientGuiController {
+    private ClientGui clientGui;
+
+    void setMainApp(ClientGui mainApp) {
+        this.clientGui = mainApp;
+    }
+}
